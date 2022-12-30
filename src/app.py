ae1816e9b08e11f9c9a7723524a5ab58b9cdeb17
Adithya Rajendran <adithyaraj@gmail.com>
2022-12-30 13:00:59 -0800

    Replaced blacklist with blocklist

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 import re
 from fnmatch import fnmatch
 
-from Gmail_App import GmailApp, audit_log
+from GmailApp import GmailApp, audit_log
 
 def handler(app):
         black_lists = open('blacklist.txt', 'r')
-        blocked_emails = black_lists.readlines()
+        blocked_emails = black_lists.read().splitlines()
         emails = app.list_inbox()
         blacklist_ids = []
         for messages in emails.get('messages'):
@@ -15,14 +15,10 @@ def handler(app):
             sender = next((header.get("value") for header in headers if header["name"] == "From"), None)
             sender_email = (re.search('<(.*)>', sender)).group(1)
             for blocked_email in blocked_emails:
-                if fnmatch(sender_email,blocked_email.strip()):
+                if fnmatch(sender_email,blocked_email):
                     blacklist_ids.append(messages.get('id'))
                     audit_log("Filter", f'{sender} : {subject}')
                     break
-                # else:
-                #     implicit deny
-                #     print("placeholder")
-                
 
         if blacklist_ids:
             app.add_quarantine(blacklist_ids)
@@ -31,5 +27,3 @@ if __name__ == "__main__":
     app = GmailApp("Test", "Account", "spamfilterbottester@gmail.com", "Quarantine")
     handler(app)
     print("Done")
-
-# test

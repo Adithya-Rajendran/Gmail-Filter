43f1c65b43fcf2804f178fc17eac63d43e196b5a
Adithya Rajendran <adithyaraj@gmail.com>
2022-12-31 14:02:11 -0800

    Added type hints

--- src/google_service.py
+++ src/google_service.py
@@ -5,7 +5,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-def AuthorizeGoogle(scopes):
+def AuthorizeGoogle(scopes: list):
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -23,4 +23,5 @@ def AuthorizeGoogle(scopes):
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+
     return build('gmail', 'v1', credentials=creds)
